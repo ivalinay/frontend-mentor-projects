5af3d4413f270fa211d016b26912779eaeac3918
ivalina <ivalina757@gmail.com>
2022-11-19 18:52:52 +0200

    solved  restaurant menu

--- SoftUni/Nov-2022/01.FirstSteps/InchestoCentimeters.js
+++
@@ -1,6 +0,0 @@
-function convertInchesToCentimeters(arg1) {
-    let inches = parseFloat(arg1);
-    let centimeters = inches * 2.54;
-    console.log(centimeters);
-}
-convertInchesToCentimeters(1);
